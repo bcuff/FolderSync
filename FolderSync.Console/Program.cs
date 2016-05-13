c560d2cd7af3cf3228892fe6591fd279419159d2
Brandon Cuff <brandon@scopely.com>
2016-05-12 18:13:12 -0700

    Created folder synchronizer

---
+++ FolderSync.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSync.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var basePath = args[0];
+            var baseSource = Path.Combine(basePath, @"WithBuddies.Web.Admin.Core");
+            var baseDest = Path.Combine(basePath, @"Dice.Web.Admin");
+            var watchers = new[]
+            {
+                new FolderSynchronizer($"{baseSource}\\Content\\",$"{baseDest}\\Content\\CoreContent\\", "*.js"),
+                new FolderSynchronizer($"{baseSource}\\Content\\",$"{baseDest}\\Content\\CoreContent\\", "*.less"),
+                new FolderSynchronizer($"{baseSource}\\Content\\",$"{baseDest}\\Content\\CoreContent\\", "*.css"),
+                new FolderSynchronizer($"{baseSource}\\Views\\",$"{baseDest}\\Views\\CoreViews\\", "*.cshtml"),
+                //new FolderSynchronizer($"{baseSource}\\fonts\\",$"{baseDest}\\fonts\\CoreFonts\\"),
+            };
+            foreach (var watcher in watchers)
+            {
+                watcher.Info += msg => System.Console.WriteLine(msg);
+                watcher.Error += msg => System.Console.Error.WriteLine(msg);
+                watcher.Start();
+                System.Console.WriteLine($"Watcher Started{Environment.NewLine}\tSource: {watcher.Source}{Environment.NewLine}\tDest: {watcher.Dest}{Environment.NewLine}\tFilter: {watcher.Filter}");
+            }
+            System.Console.WriteLine("Press enter to stop.");
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ FolderSync/FolderSynchronizer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSync
+{
+    public class FolderSynchronizer : IDisposable
+    {
+        public string Source { get; }
+        public string Dest { get; }
+        public string Filter { get; }
+        FileSystemWatcher _watcher;
+
+        public FolderSynchronizer(string source, string dest, string filter = null)
+        {
+            Source = Path.GetFullPath(source);
+            Dest = Path.GetFullPath(dest);
+            Filter = filter;
+
+            if (!Source.EndsWith("\\")) Source += "\\";
+        }
+
+        public void Start()
+        {
+            if (_watcher != null) throw new InvalidOperationException("Already started.");
+            _watcher = new FileSystemWatcher(Source, Filter ?? "*.*")
+            {
+                NotifyFilter = NotifyFilters.FileName,
+                IncludeSubdirectories = true,
+            };
+            _watcher.Changed += OnChange;
+            _watcher.Created += OnChange;
+            _watcher.Deleted += OnChange;
+            _watcher.Renamed += OnRename;
+            _watcher.EnableRaisingEvents = true;
+        }
+
+        public event Action<string> Info;
+
+        public event Action<string> Error;
+
+        private string GetDestPath(string sourcePath)
+        {
+            var subpath = sourcePath.Substring(Source.Length);
+            return Path.Combine(Dest, subpath);
+        }
+
+        private void Log(bool error, string message)
+        {
+            var e = error ? Error : Info;
+            e?.Invoke(message);
+        }
+
+        private void OnChange(object sender, FileSystemEventArgs e)
+        {
+            var destpath = GetDestPath(e.FullPath);
+            if (e.ChangeType.HasFlag(WatcherChangeTypes.Deleted))
+            {
+                try
+                {
+                    File.Delete(destpath);
+                }
+                catch (Exception ex)
+                {
+                    Log(true, $"Failed to delete {destpath} - {ex}");
+                    return;
+                }
+                Log(false, $"Deleted {destpath}");
+                return;
+            }
+            if (e.ChangeType.HasFlag(WatcherChangeTypes.Changed) || e.ChangeType.HasFlag(WatcherChangeTypes.Created))
+            {
+                try
+                {
+                    File.Copy(e.FullPath, destpath, true);
+                }
+                catch (Exception ex)
+                {
+                    Log(true, $"Failed to copy {e.FullPath} to {destpath} - {ex}");
+                    return;
+                }
+                Log(false, $"Copied {e.FullPath} to {destpath}");
+            }
+        }
+
+        private void OnRename(object sender, RenamedEventArgs e)
+        {
+            var olddest = GetDestPath(e.OldFullPath);
+            var newdest = GetDestPath(e.FullPath);
+            if (File.Exists(olddest))
+            {
+                try
+                {
+                    File.Move(olddest, newdest);
+                }
+                catch (Exception ex)
+                {
+                    Log(true, $"Failed to rename {olddest} to {newdest} - {ex}");
+                    return;
+                }
+                Log(false, $"Moved {olddest} to {newdest}.");
+                return;
+            }
+            try
+            {
+                File.Copy(e.FullPath, newdest, true);
+            }
+            catch (Exception ex)
+            {
+                Log(true, $"Failed to copy {e.FullPath} to {newdest} - {ex}");
+                return;
+            }
+            Log(false, $"Copied {e.FullPath} to {newdest}");
+        }
+
+        public void Dispose()
+        {
+            _watcher.Dispose();
+        }
+    }
+}
